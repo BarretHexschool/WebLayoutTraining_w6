dbec559f4f3cb4660d1abb1228565dbf816956df
barret0720 <jianhong.liou@gmail.com>
2022-09-14 13:03:41 +0800

    First Upload

---
+++ app/assets/js/all.js
@@ -0,0 +1,16 @@
+$(function() {
+  console.log('Coding Home 您最好的程式家教');
+
+  var swiperFeedback = new Swiper(".feedback-content", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".feedback-btn-next",
+      prevEl: ".feedback-btn-prev"
+    },
+    pagination: {
+      el: ".feedback-pagination",
+      clickable: true
+    }
+  });
+
+});
